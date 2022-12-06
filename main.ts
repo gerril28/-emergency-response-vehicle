2ff0e21347a349bcbfccfd93be11feb5fc1fafcc
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-12-06 09:32:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-    	
-    } else {
-    	
-    }
-})
+function green () {
+	
+}
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(0)
-})
-input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
 })
 radio.setGroup(39)
