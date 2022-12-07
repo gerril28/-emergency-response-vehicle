adaee181b554ad1aab519c9efe7ec698c29eec40
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-12-07 09:32:48 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-function green () {
-	
-}
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
 radio.setGroup(39)
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
 	
 })
